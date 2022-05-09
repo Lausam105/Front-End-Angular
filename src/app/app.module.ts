ee4d210f4d792bbe30ba7828cfe44372b1989b79
Lausam105 <karimganimi22@gmil.com>
2022-05-09 11:40:23 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module.ts/app-routing.module.ts.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { BarraDeNavegacionComponent } from './componentes/barra-de-navegacion/barra-de-navegacion.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    BarraDeNavegacionComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    
+  
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
